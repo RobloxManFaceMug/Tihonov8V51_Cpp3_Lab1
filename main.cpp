5e712dbc8312c144bc4e5ffbe93bbb017f0fdab3
RobloxManFaceMug <nbahtnxohob@gmail.com>
2026-09-11 17:52:45 +0700

    And fleas

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include "lib.h"
+#include "build/version.h"
 
 #include <iostream>
 
 int main (int, char **) {
-    std::cout << "Hello, world! Version " << version() << std::endl;
+    std::cout << "Hello, world! Version 1.0." << GIT_RELEASE_VERSION[1] << std::endl;
     return 0;
 }
 
